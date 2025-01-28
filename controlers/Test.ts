9e71db7c9985af70bd5310b36cae04cceac28e34
moses mwangi <moses.mwangi.me@gmail.com>
2025-01-28 15:04:09 +0300

    coomit

--- controlers/Test.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-const getTest = async (req: Request, res: Response, next: NextFunction) => {
-  console.log("JUST FOR TESTING");
-
-  res.status(200).json({ status: "success", data: "None" });
-};
-
-export default getTest;
